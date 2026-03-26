87b2fb4cf7c6689e97bb59231dae2726ad688a88
Venkey2007 <venkateswarareddythatigotla@gmail.com>
2026-03-26 15:21:43 +0530

    UC6 - Greeting using enhanced for loop and substring method

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -6,10 +6,17 @@ public class HelloWorldApp {
             System.out.println("Hello, World!");
         } 
         else {
-            for (String name : args) {
-                System.out.println("Hello, " + name + "!");
-            }
-        }
 
+            String names = "";
+
+            for (String name : args) {
+                names += name + ", ";
+            }
+
+            // remove last comma and space
+            names = names.substring(0, names.length() - 2);
+
+            System.out.println("Hello, " + names + "!");
+        }
     }
 }
