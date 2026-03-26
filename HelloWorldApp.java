24d8ca9771ab267299b1d6fbe7e757e39859f33e
Venkey2007 <venkateswarareddythatigotla@gmail.com>
2026-03-26 15:10:29 +0530

    UC4 - Display Hello for multiple command-line arguments

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -2,12 +2,14 @@ public class HelloWorldApp {
 
     public static void main(String[] args) {
 
-        String name = "World";   // default value
-
-        if (args.length > 0) {
-            name = args[0];
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } 
+        else {
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
         }
 
-        System.out.println("Hello " + name);
     }
 }
